44c7a01dca9788d8ae93ae58a7582d9a6590e88a
LukasPecak <lukas.pecak@gmail.com>
2019-07-28 23:15:12 +0200

    Addad detektion for line separator in the input.

--- src/main/java/org/lukas/javach/model/Document.java
+++ src/main/java/org/lukas/javach/model/Document.java
@@ -7,13 +7,46 @@ package org.lukas.javach.model;
  */
 class Document {
 
+    private static final int CARRIAGE_RETURN = 0x0D;
+    private static final int LINE_FEED = 0x0A;
+    static final byte[] WINDOWS_LINE_SEPARATOR = {CARRIAGE_RETURN, LINE_FEED};
+    static final byte[] UNIX_LINE_SEPARATOR = {LINE_FEED};
+    static final byte[] OLD_MAC_LINE_SEPARATOR = {CARRIAGE_RETURN};
+
     private byte[] bytes;
+    private byte[] lineSeparator;
 
     Document(byte[] bytes) {
+        if (bytes == null) {
+            throw new IllegalArgumentException("Cannot initialize a document with a null array of bytes");
+        }
         this.bytes = bytes;
+
+        lineSeparator = resolveLineSeparator(bytes);
+    }
+
+    private byte[] resolveLineSeparator(byte[] bytes) {
+        if (bytes.length == 0) {
+            return System.lineSeparator().getBytes();
+        }
+        for (int i = 0; i < bytes.length - 1; i++) {
+            if (bytes[i] == CARRIAGE_RETURN) {
+                if (bytes[i + 1] == LINE_FEED) {
+                    return WINDOWS_LINE_SEPARATOR;
+                }
+                return OLD_MAC_LINE_SEPARATOR;
+            } else if (bytes[i] == LINE_FEED) {
+                return UNIX_LINE_SEPARATOR;
+            }
+        }
+        return System.lineSeparator().getBytes();
     }
 
     byte[] getBytes() {
         return bytes;
+    }
+
+    byte[] getLineSeparator() {
+        return lineSeparator;
     }
 }
--- src/test/java/org/lukas/javach/model/DocumentTest.java
+++ src/test/java/org/lukas/javach/model/DocumentTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.lukas.javach.model.Document.*;
 
 /**
  * Created by Lukas on 28.07.2019.
@@ -32,5 +33,74 @@ public class DocumentTest {
         document = new Document(data.getBytes());
 
         assertThat(document.getBytes(), is(equalTo(data.getBytes())));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructor_shouldThrowIllegalArgumentException_whenTryToInitializeWithNullValue() {
+        new Document(null);
+    }
+
+    @Test
+    public void getLineSeparator_shouldReturnTheLineSeparatorUsedInTheDocument_whenDocumentHasMultipleLines() {
+        // GIVEN
+        String windowsLineSeparator = "\r\n";
+        document = new Document(("This is the first line" + windowsLineSeparator + "This is the second line").getBytes());
+
+        // WHEN
+        byte[] separator = document.getLineSeparator();
+
+        // THEN
+        assertThat(separator, is(equalTo(WINDOWS_LINE_SEPARATOR)));
+    }
+
+    @Test
+    public void getLineSeparator_shouldReturnTheSystemDefaultLineSeparator_whenDocumentIsEmpty() {
+        // GIVEN
+        byte[] systemSeparator = System.lineSeparator().getBytes();
+
+        // WHEN
+        byte[] separator = document.getLineSeparator();
+
+        // THEN
+        assertThat(separator, is(equalTo(systemSeparator)));
+    }
+
+    @Test
+    public void getLineSeparator_shouldReturnTheSystemDefaultLineSeparator_whenDocumentHasOnlyOneLine() {
+        // GIVEN
+        document = new Document("One line test only. Without line separator".getBytes());
+        byte[] systemSeparator = System.lineSeparator().getBytes();
+
+        // WHEN
+        byte[] separator = document.getLineSeparator();
+
+        // THEN
+        assertThat(separator, is(equalTo(systemSeparator)));
+    }
+
+    @Test
+    public void getLineSeparator_shouldReturnTheUnixLineSeparator_whenDocumentHasMultipleLinesAndUnixFormatting() {
+        // GIVEN
+        String unixLineSeparator = "\n";
+        document = new Document(("This is the first line" + unixLineSeparator + "This is the second line").getBytes());
+
+        // WHEN
+        byte[] separator = document.getLineSeparator();
+
+        // THEN
+        assertThat(separator, is(equalTo(UNIX_LINE_SEPARATOR)));
+    }
+
+    @Test
+    public void getLineSeparator_shouldReturnTheOldMacLineSeparator_whenDocumentHasMultipleLinesAndOldMacFormatting() {
+        // GIVEN
+        String oldMacLineSeparator = "\r";
+        document = new Document(("This is the first line" + oldMacLineSeparator + "This is the second line").getBytes());
+
+        // WHEN
+        byte[] separator = document.getLineSeparator();
+
+        // THEN
+        assertThat(separator, is(equalTo(OLD_MAC_LINE_SEPARATOR)));
     }
 }
